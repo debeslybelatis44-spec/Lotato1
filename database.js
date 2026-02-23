48d85451e610472b709e5af87b69a77187d5c8aa
debeslybelatis44-spec <debeslybelatis44@gmail.com>
2026-02-22 21:55:43 -0500

    Add files via upload

--- database.js
+++ database.js
@@ -1,14 +1,14 @@
 const { Sequelize } = require('sequelize');
 
-// Utilisation de DATABASE_URL (ex: mysql://user:pass@host:port/db)
+// Utilisation de DATABASE_URL (ex: postgresql://user:pass@host:port/db)
 // Les options SSL sont déjà gérées via dialectOptions
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'mysql',
+    dialect: 'postgres',
     logging: false,
     dialectOptions: {
         ssl: {
             require: true,
-            rejectUnauthorized: true // Mettre false si vous avez des problèmes de certificat (ex: avec TiDB)
+            rejectUnauthorized: true // Mettre false si vous avez des problèmes de certificat (ex: avec Neon)
         }
     },
     pool: {
